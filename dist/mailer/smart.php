c20f903bf2b60dce583657341c4a9233aa2a0b26
unknown <flowerrosa76@gmail.com>
2024-05-12 18:29:30 +0900

    updated

--- dist/mailer/smart.php
+++ dist/mailer/smart.php
@@ -14,7 +14,7 @@ $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
 $mail->Username = 'endohanna@gmail.com';                 // Наш логин
-$mail->Password = 'RabochiiMail22';                           // Наш пароль от ящика
+$mail->Password = 'donh ehqa bjjb qqmq';                           // Наш пароль от ящика
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465;                                    // TCP port to connect to
  
--- src/js/script.js
+++ src/js/script.js
@@ -22,7 +22,7 @@ $(window).load(function () {
       repeatDelay:0.1,
       ease: "power1.in",
   })
-   gsap.from('.skills_logo',{
+  gsap.from('.skills__logo',{
       opacity:0,
       duration:2,
       stagger:0.7,
@@ -37,8 +37,37 @@ $(window).load(function () {
       this.classList.toggle('active');
       navigation.classList.toggle('active');
   
-  }
-  
+    }    
+    
+    
+    // для отправки писем
+    
+    $(document).ready(function(){
+    
+    $('form').submit(function(e) {
+    e.preventDefault();
+    if (!$(this).valid()){
+      return;
+    }
+    $.ajax({
+        type: "POST",
+        url: "mailer/smart.php",
+        data: $(this).serialize()
+    }).done(function() {
+        $(this).find("input").val("");
+        $('.overlay, #thanks').fadeIn('slow');
+        $('form').trigger('reset');
+    });
+    return false;
+    });
+    // для крестика
+    $('.modal__close').on('click', function() {
+      $('.overlay,#thanks').fadeOut('slow');
+    });
+    
+    });
+    
+
   // Show
   
   particlesJS("particles-js", {
@@ -48,26 +77,26 @@ $(window).load(function () {
         density: {
           enable: true,
           value_area: 631.3280775270874
-        }
-      },
+        }  
+      },  
       color: {
         value: "#fff"
-      },
+      },  
       shape: {
         type: "circle",
         stroke: {
           width: 0,
           color: "#000000"
-        },
+        },  
         polygon: {
           nb_sides: 5
-        },
+        },  
         image: {
           src: "img/github.svg",
           width: 100,
           height: 100
-        }
-      },
+        }  
+      },  
       opacity: {
         value: 0.4,
         random: true,
@@ -76,8 +105,8 @@ $(window).load(function () {
           speed: 1,
           opacity_min: 0.1,
           sync: false
-        }
-      },
+        }  
+      },  
       size: {
         value: 5,
         random: true,
@@ -86,15 +115,15 @@ $(window).load(function () {
           speed: 40,
           size_min: 0.1,
           sync: false
-        }
-      },
+        }  
+      },  
       line_linked: {
         enable: false,
         distance: 500,
         color: "#ffffff",
         opacity: 0.4,
         width: 2
-      },
+      },  
       move: {
         enable: true,
         speed: 1.5,
@@ -107,52 +136,50 @@ $(window).load(function () {
           enable: false,
           rotateX: 600,
           rotateY: 1200
-        }
-      }
-    },
+        }  
+      }  
+    },  
     interactivity: {
       detect_on: "canvas",
       events: {
         onhover: {
           enable: false,
           mode: "bubble"
-        },
+        },  
         onclick: {
           enable: true,
           mode: "repulse"
-        },
+        },  
         resize: true
-      },
+      },  
       modes: {
         grab: {
           distance: 400,
           line_linked: {
             opacity: 0.5
-          }
-        },
+          }  
+        },  
         bubble: {
           distance: 400,
           size: 4,
           duration: 0.3,
           opacity: 1,
           speed: 3
-        },
+        },  
         repulse: {
           distance: 200,
           duration: 0.4
-        },
+        },  
         push: {
           particles_nb: 4
-        },
+        },  
         remove: {
           particles_nb: 2
-        }
-      }
-    },
+        }  
+      }  
+    },  
     retina_detect: true
-  });
-
-
+  });  
   try {
     var count_particles, stats, update;
     stats = new Stats();
@@ -168,13 +195,13 @@ $(window).load(function () {
         if (
             window.pJSDom[0].pJS.particles &&
             window.pJSDom[0].pJS.particles.array
-        ) {
+        ) {  
             count_particles.innerText = window.pJSDom[0].pJS.particles.array.length;
-        }
+        }    
         requestAnimationFrame(update);
-    };
+    };    
     requestAnimationFrame(update);
-} catch (e) {}
+} catch (e) {}    
 
 
 
@@ -188,40 +215,14 @@ $(window).load(function () {
   // document.body.appendChild(stats.domElement);
   // count_particles = document.querySelector(".js-count-particles");
   // update = function() {
-  //   stats.begin();
+  //   stats.begin();  
   //   stats.end();
   //   if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) {
-  //     count_particles.innerText = window.pJSDom[0].pJS.particles.array.length;
+  //     count_particles.innerText = window.pJSDom[0].pJS.particles.array.length;  
   //   }
   //   requestAnimationFrame(update);
   // };
   // requestAnimationFrame(update);
-  
-  // для отправки писем
-
-$(document).ready(function(){
- 
- $('form').submit(function(e) {
-  e.preventDefault();
-  if (!$(this).valid()){
-    return;
-  }
-  $.ajax({
-      type: "POST",
-      url: "mailer/smart.php",
-      data: $(this).serialize()
-  }).done(function() {
-      $(this).find("input").val("");
-     
-      $('form').trigger('reset');
-  });
-  return false;
-});
-
-
- });
-  
-
 
   
 
